d7e09006956b6ee2e5b51221e1c0a74812513b65
aditya.jagtiani <aditya.jagtiani@appdynamics.com>
2019-11-27 11:35:38 -0800

    Refactoring

--- src/test/java/com/appdynamics/extensions/logmonitor/processors/LogFileManagerTest.java
+++ src/test/java/com/appdynamics/extensions/logmonitor/processors/LogFileManagerTest.java
@@ -10,14 +10,19 @@ package com.appdynamics.extensions.logmonitor.processors;
 
 import com.appdynamics.extensions.AMonitorJob;
 import com.appdynamics.extensions.conf.MonitorContextConfiguration;
+import com.appdynamics.extensions.eventsservice.EventsServiceDataManager;
+import com.appdynamics.extensions.logmonitor.LogEvent;
 import com.appdynamics.extensions.logmonitor.config.FilePointer;
 import com.appdynamics.extensions.logmonitor.config.Log;
+import com.appdynamics.extensions.logmonitor.config.SearchPattern;
 import com.appdynamics.extensions.logmonitor.config.SearchString;
 import com.appdynamics.extensions.logmonitor.metrics.LogMetrics;
 import com.appdynamics.extensions.logmonitor.util.LogMonitorUtil;
 import com.appdynamics.extensions.metrics.Metric;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang.StringUtils;
+import org.bitbucket.kienerj.OptimizedRandomAccessFile;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -27,7 +32,9 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
+import static com.appdynamics.extensions.logmonitor.util.Constants.SCHEMA_NAME;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
@@ -610,6 +617,29 @@ public class LogFileManagerTest {
                 metrics.get("TestLog|File size (Bytes)").getMetricValue());
     }
 
+    @Test
+    public void testLogEventGeneratorWithOffset() throws Exception {
+        EventsServiceDataManager eventsServiceDataManager = Mockito.mock(EventsServiceDataManager.class);
+        Mockito.when(eventsServiceDataManager.retrieveSchema(SCHEMA_NAME)).thenReturn("Hello world");
+        Log log = new Log();
+        log.setDisplayName("TestLog");
+        log.setLogDirectory("src/test/resources/");
+        log.setLogName("test-log-4-events-service.log");
+
+        SearchPattern searchPattern = new SearchPattern("Test Patterns", Pattern.compile("1"), false, false );
+
+        FilePointer filePointer = new FilePointer();
+        filePointer.setFilename(log.getLogDirectory() + log.getLogName());
+
+        OptimizedRandomAccessFile randomAccessFile = new OptimizedRandomAccessFile(new File("src/test/resources/test-log-4-events-service.log"), "r");
+
+        int offset = 5;
+
+        LogEventsProcessor classUnderTest = new LogEventsProcessor(eventsServiceDataManager, offset, log);
+        LogEvent logEvent = classUnderTest.processLogEvent(searchPattern, randomAccessFile, "");
+
+        Assert.assertEquals("1\n1\n2\n3\n4\n", logEvent.getLogMatch());
+    }
     
 
     
